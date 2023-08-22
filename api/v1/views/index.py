69713b4cbfa4357a3a9e988b94e0b2b086606ba1
Gabr1el20 <6327@holbertonstudents.com>
2023-08-22 11:37:14 -0700

    Task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,29 @@
 """Returns a jsoned status"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route("/status")
 def jsoned():
     return jsonify({"status": "OK"})
+
+
+cls_dict = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
+
+
+@app_views.route("/stats")
+def stats():
+    """_summary_
+    """
+    dicted = {}
+    for key, value in cls_dict.items():
+        dicted[key] = storage.count(value)
+    return jsonify(dicted)
